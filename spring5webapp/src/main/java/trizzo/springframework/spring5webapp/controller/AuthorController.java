8fbb0ccc4937e5ace2879c2eed472f2331d6cda4
trizzo <trizzo@payfone.com>
2019-04-05 13:18:32 -0600

    Formatting cleanup.

--- spring5webapp/src/main/java/trizzo/springframework/spring5webapp/controller/AuthorController.java
+++ spring5webapp/src/main/java/trizzo/springframework/spring5webapp/controller/AuthorController.java
@@ -10,18 +10,15 @@ import trizzo.springframework.spring5webapp.repositories.AuthorRepository;
 public class AuthorController {
 
 	private AuthorRepository authorRepository;
-	
-	public AuthorController(AuthorRepository authorRepository)
-	{
-		this.authorRepository = authorRepository; 
-	}
-	
-	@RequestMapping("/authors")
-	public String getAuthors(Model model)
-	{
-		model.addAttribute("authors", authorRepository.findAll()); 
-		
-		return "authors";
+
+	public AuthorController(AuthorRepository authorRepository) {
+		this.authorRepository = authorRepository;
 	}
 
+	@RequestMapping("/authors")
+	public String getAuthors(Model model) {
+		model.addAttribute("authors", authorRepository.findAll());
+
+		return "authors";
+	}
 }
