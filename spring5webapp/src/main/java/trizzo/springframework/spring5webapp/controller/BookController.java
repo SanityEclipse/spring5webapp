39586a31b0bf88fe131b11255ac0347cb3706725
trizzo <trizzo@payfone.com>
2019-04-05 11:07:27 -0600

    Added controller package and BookController.

---
+++ spring5webapp/src/main/java/trizzo/springframework/spring5webapp/controller/BookController.java
@@ -0,0 +1,26 @@
+package trizzo.springframework.spring5webapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import trizzo.springframework.spring5webapp.repositories.BookRepository;
+
+@Controller
+public class BookController {
+	
+	private BookRepository bookRepository; 
+	
+	public BookController(BookRepository bookRepository)
+	{
+		this.bookRepository = bookRepository; 
+	}
+	
+	@RequestMapping("/books")
+	public String getBooks(Model model) {
+		
+		model.addAttribute("books", bookRepository.findAll());
+		
+		return "books"; // tells SpringMVC to associate with view called 'books'
+	}
+}
