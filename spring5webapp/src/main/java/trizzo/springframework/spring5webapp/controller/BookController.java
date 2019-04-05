e44fa3c42541f02519ee931a7e00805d94e9dbb2
trizzo <trizzo@payfone.com>
2019-04-05 11:35:25 -0600

    Added thymeleaf view.

--- spring5webapp/src/main/java/trizzo/springframework/spring5webapp/controller/BookController.java
+++ spring5webapp/src/main/java/trizzo/springframework/spring5webapp/controller/BookController.java
@@ -8,19 +8,18 @@ import trizzo.springframework.spring5webapp.repositories.BookRepository;
 
 @Controller
 public class BookController {
-	
-	private BookRepository bookRepository; 
-	
-	public BookController(BookRepository bookRepository)
-	{
-		this.bookRepository = bookRepository; 
+
+	private BookRepository bookRepository;
+
+	public BookController(BookRepository bookRepository) {
+		this.bookRepository = bookRepository;
 	}
-	
+
 	@RequestMapping("/books")
 	public String getBooks(Model model) {
-		
+
 		model.addAttribute("books", bookRepository.findAll());
-		
+
 		return "books"; // tells SpringMVC to associate with view called 'books'
 	}
 }
